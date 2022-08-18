c82492e63f4d2d8b5761fec09a506bd1458fef50
Andrés Hernández <andres@tonejito.org>
2022-08-18 16:23:25 -0500

    Add wait command

--- update.sh
+++ update.sh
@@ -24,6 +24,10 @@ RHT_OCP4_MASTER_API=https://api.ocp4.example.com:6443
 
 set -x
 
+ssh lab@utility ~/wait.sh
+
+sleep 2
+
 oc login --insecure-skip-tls-verify\
   -u ${RHT_OCP4_DEV_USER} \
   -p ${RHT_OCP4_DEV_PASSWORD} \
